e92499b01e7bcae5a8f92d984accf51f471a46f3
Lucas <58535439+Lucas-bit-maker@users.noreply.github.com>
2023-12-26 14:14:09 -0800

    make a 16 by 16 square grid

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const container = document.getElementById('grid-container');
 
+    // Create a 16x16 grid
+    for (let i = 0; i < 16 * 16; i++) {
+        const square = document.createElement('div');
+        square.classList.add('square');
+        container.appendChild(square);
+    }
+});
